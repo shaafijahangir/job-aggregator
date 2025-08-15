28d48f724a404532259fa267d9db3d624623a4a3
M Shaafi Jahangir <97818517+shaafijahangir@users.noreply.github.com>
2025-08-15 04:45:22 -0700

    scripts/data/allJobs.json

--- scripts/scrapeJobPortals.js
+++ scripts/scrapeJobPortals.js
@@ -1,5 +1,5 @@
-import { scrapeCivicJobs } from './jobPortals/civicjobsScraper.js';
-import { scrapeRemotiveJobs } from './jobPortals/remotiveScraper.js';
+import { scrapeCivicJobs } from './jobPortals/civicJobsScraper.js';
+// import { scrapeRemotiveJobs } from './jobPortals/remotiveScraper.js';
 import fs from 'fs';
 import path from 'path';
 
@@ -7,13 +7,16 @@ async function runAllScrapers() {
   console.log('🔄 Starting all job scrapers...');
 
   const civicJobs = await scrapeCivicJobs();
-  //const remotiveJobs = await scrapeRemotiveJobs();
+  // const remotiveJobs = await scrapeRemotiveJobs();
 
-  //const allJobs = [...civicJobs, ...remotiveJobs];
+  // const allJobs = [...civicJobs, ...remotiveJobs];
+  const allJobs = civicJobs;
 
   const outputPath = path.resolve('data/allJobs.json');
-  fs.writeFileSync(outputPath, JSON.stringify(civicJobs, null, 2));
-  console.log(`✅ Merged ${allJobs.length} jobs into data/allJobs.json`);
+  fs.mkdirSync(path.dirname(outputPath), { recursive: true }); // make sure /scripts/data exists
+  fs.writeFileSync(outputPath, JSON.stringify(allJobs, null, 2));
+
+  console.log(`✅ Saved ${allJobs.length} jobs to ${outputPath}`);
 }
 
 runAllScrapers();
